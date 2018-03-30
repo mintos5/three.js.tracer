46e91c64fb35d0698ca6945d3de7e6164153bb56
Michal Skuta <michal.skuta@gmail.com>
2018-03-31 01:57:29 +0200

    added drawing curved lines from geo position (lat,lon)

---
+++ js/lineGenerator.js
@@ -0,0 +1,79 @@
+function positionOnSphere(lat, lon, radius) {
+    var phi   = (90-lat)*(Math.PI/180),
+        theta = (lon+180)*(Math.PI/180),
+        x = -((radius) * Math.sin(phi)*Math.cos(theta)),
+        z = ((radius) * Math.sin(phi)*Math.sin(theta)),
+        y = ((radius) * Math.cos(phi));
+
+    return new THREE.Vector3(x,y,z);
+}
+
+
+function LineGenerator(radius, scene) {
+    this.sphereRadius = radius;
+    this.scene = scene;
+    this.raycaster = new THREE.Raycaster();
+}
+
+LineGenerator.prototype.generateLine = function (lat1, lon1, lat2, lon2) {
+    // var dotGeometry = new THREE.Geometry();
+    // dotGeometry.vertices.push(positionOnSphere(lat,lon,this.sphereRadius));
+    // var dotMaterial = new THREE.PointsMaterial( { size: 10, sizeAttenuation: false } );
+    // var dot = new THREE.Points( dotGeometry, dotMaterial );
+    // this.scene.add( dot );
+
+    var startPosition = positionOnSphere(lat1,lon1,this.sphereRadius);
+    var endPosition = positionOnSphere(lat2,lon2,this.sphereRadius);
+
+    //create curve to get center of the line
+    var calculateCurve = new THREE.LineCurve3(startPosition,endPosition);
+    var middlePosition = calculateCurve.getPoint(0.5);
+
+    //find position of centre on sphere with raycasting
+    this.raycaster = new THREE.Raycaster();
+    this.raycaster.set(this.scene.getWorldPosition(),middlePosition.normalize());
+    var intersects = this.raycaster.intersectObject(this.scene,false);
+    if (intersects.length > 0) {
+        //position on sphere of the center of line
+        middlePosition.copy(intersects[0].point);
+        //move it litle above sphere
+        var moveVector = new THREE.Vector3().copy(middlePosition);
+        moveVector.normalize().multiplyScalar(6);
+        middlePosition.add(moveVector);
+    }
+    var curve = new THREE.QuadraticBezierCurve3(
+        startPosition,
+        middlePosition,
+        endPosition
+    );
+    //CREATE THE LINE
+    //get three.js like
+    var points = curve.getPoints( 18 );
+    var color = new THREE.Color( 0xff0000 );
+    var pointsArray = [];
+    var colors = [];
+
+    for (var i =0; i < points.length; i++) {
+        pointsArray.push(points[i].x,points[i].y,points[i].z);
+        colors.push(color.r,color.g,color.b);
+    }
+
+    var geometryL = new THREE.LineGeometry();
+    geometryL.setPositions( pointsArray );
+    geometryL.setColors(colors);
+
+    var materialL = new THREE.LineMaterial( {
+        color: 0xffffff,
+        linewidth: 7, // in pixels
+        vertexColors: THREE.VertexColors,
+        //resolution:  // to be set by renderer, eventually
+        dashed: false
+    }  );
+    materialL.resolution.set( window.innerWidth, window.innerHeight );
+
+    var curveObject = new THREE.Line2( geometryL, materialL );
+    curveObject.computeLineDistances();
+    curveObject.scale.set( 1, 1, 1 );
+    this.scene.add(curveObject);
+    //NOTICE that objects are added to sphere and not to scene, maybe i the furure we will need update positions
+};
---
+++ js/main.js
@@ -0,0 +1,72 @@
+var sphere;
+var radiusSize = 20;
+function Main() {
+    this.scene = new THREE.Scene();
+    this.camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+    this.renderer = new THREE.WebGLRenderer();
+    this.lineGenerator = new LineGenerator(radiusSize,this.scene);
+    this.stats = new Stats();
+}
+Main.prototype.init = function () {
+    this.renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( this.renderer.domElement );
+
+    var light = new THREE.AmbientLight( 0x404040 ); // soft white light
+    this.scene.add( light );
+
+    var directionalLight = new THREE.DirectionalLight( 0xffffff, 1 );
+    directionalLight.position.copy(new THREE.Vector3(10,7,0));
+    this.scene.add( directionalLight );
+    //var helper = new THREE.DirectionalLightHelper( directionalLight, 5 );
+
+    //this.scene.add( helper );
+
+
+    this.stats.showPanel( 0 ); // 0: fps, 1: ms, 2: mb, 3+: custom
+    document.body.appendChild( this.stats.dom );
+
+    //generate sphere
+    var geometry = new THREE.SphereGeometry( radiusSize, 50, 50 );
+    var material = new THREE.MeshPhongMaterial();
+    material.side = THREE.DoubleSide;
+    //material.wireframe = true;
+    var loaderDiffuse = new THREE.TextureLoader();
+    loaderDiffuse.load("textures/earthmap8k.jpg", function (texture) {
+        material.map = texture;
+        material.needsUpdate = true;
+    });
+    var loaderBump = new THREE.TextureLoader();
+    loaderBump.load("textures/earthbump1k.jpg", function (texture) {
+        material.bumpMap = texture;
+        material.bumpScale = 0.001;
+        material.needsUpdate = true;
+    });
+    var loaderSpecular = new THREE.TextureLoader();
+    loaderSpecular.load("textures/earthspec1k.jpg", function (texture) {
+        material.specularMap = texture;
+        material.needsUpdate = true;
+    });
+    sphere = new THREE.Mesh( geometry, material );
+    this.lineGenerator.scene = sphere;
+    this.scene.add( sphere );
+
+
+    //create lineGenerator
+    this.lineGenerator.generateLine(0,0,48.143889,17.109722);
+
+    this.camera.position.z = 40;
+};
+
+Main.prototype.animate = function () {
+    requestAnimationFrame( this.animate.bind(this));
+    this.stats.begin();
+    //cube.rotation.x += 0.05;
+    sphere.rotation.y += 0.005;
+
+    this.renderer.render(this.scene, this.camera);
+    this.stats.end();
+};
+
+Main.prototype.addLine = function (object) {
+
+};
